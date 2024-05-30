02f4d48a0f5d2cce2f6300ddca8a7e170a7da565
nahum22 <nahum.nahum@gmail.com>
2024-05-30 15:57:12 +0300

    first

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import Title from "./title";
+import menu from "./data";
+import { useState } from "react";
+import Menu from "./Menu";
+import Categories from "./Categories";
+
+const tempCategories = menu.map((item) => item.category);
+
+const allCategories = ["all", ...new Set(tempCategories)];
+const App = () => {
+  const [menuItems, setMenuItems] = useState(menu);
+  const [categories, setCategories] = useState(allCategories);
+  const filterItems = (category) => {
+    console.log(category);
+    if (category === "all") {
+      setMenuItems(menu);
+      return;
+    }
+
+    const newItems = menu.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
+  return (
+    <main>
+      <section className="menu">
+        <Title text="our menu" />
+        <Categories categories={categories} filterItems={filterItems} />
+
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
+};
+export default App;
